7515f89093583a91e838395cc1bc9ce32ba7d564
Remberto <rembertobeltran@icloud.com>
2022-05-08 15:28:53 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+//Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            accept: "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        })
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(object => {
+        document.body.innerHTML = object["id"];
+    })
+    .catch(error => {
+        alert("Bad things! Ragnarők!");
+        document.body.innerHTML = error.message;
+    })
+}
